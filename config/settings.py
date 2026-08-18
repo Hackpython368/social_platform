a0d299fcfc0f2b12fe7177aa749a41542447434d
Hackpython368 <prakashvidya368@gmail.com>
2026-08-18 22:52:59 +0530

    Configure static file for production

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jxk92!f4=zt%a&(xr3pd6py@g=8%#mb!_s4f+wyc*21+l(0t*!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['prescriptive-nonstrategically-mae.ngrok-free.dev','localhost','127.0.0.1','social-platform-pcfv.onrender.com']
 
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -130,6 +131,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 MEDIA_ROOT = BASE_DIR / "media"
 
